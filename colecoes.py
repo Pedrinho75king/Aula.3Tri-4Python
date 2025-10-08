845dfd8cb258e60ddf578cbb05ebead5c5f39f05
Pedrinho75king <pedroyan@colegiopaulodetarso.com.br>
2025-10-08 16:13:58 +0000

    Yep

---
+++ colecoes.py
@@ -0,0 +1,6 @@
+lista = [5.7, 1.94, 2.6, 3, 3.43]
+print(max(lista))
+print(min(lista))
+print(sorted(lista))
+print(len(lista))
+print(sum(lista))
---
+++ conjuntos.py
@@ -0,0 +1,14 @@
+# 1. Lista de compras
+compras = {"energético", "lichia", "chocolate", "picanha"}
+# 2. Coordenadas geográficas (x,y)
+coordenadas_seul = (37.5665, 126.9780)
+# 3. CPFs de clientes
+cpfs = {'123.456.789-09', '987.654.321-00', '321.654.987-11', '741.852.963-22'}
+# 4. Notas dos alunos
+notas = {'7.23', '4.1', '5.76', '9.5', '7.6', '6.8', '8.47'}
+# 5. Tarefas
+tarefas = {"Logarítimo", "HIV", "Revolução Francesa", "Barroco"}
+# 6. Meses do Ano
+meses = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+# 7. Primeiro nome de alunos
+alunos = {'Pedro', 'Giovanna', 'Lucas', 'Davi', 'Lilian', 'Daniel', 'Rafael', 'Cris'}
